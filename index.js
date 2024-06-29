9c9ffaa0862cd049d496db76f3836d28ac42c853
Avinash Kumar <avinashkumarprasad72@gmail.com>
2024-06-29 19:06:18 +0530

    minor change

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const y = document.getElementById('checkbox')
 function fun(){
     if(y.checked==true){
         console.log(y.checked)
-        x.classList.add('notshow')
+        x.classList.add('show')
     }else{
-        x.classList.remove('notshow')
+        x.classList.remove('show')
     }
 }
