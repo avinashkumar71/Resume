a8d5a88b294468cec513f87b3ede0c3024859259
Avinash Kumar <avinashkumarprasad72@gmail.com>
2024-07-02 22:00:32 +0530

    add sections

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const x = document.getElementsByClassName('navbar_list')[0]
 const y = document.getElementById('checkbox')
+let category_element = document.getElementsByClassName('category_tab')
+let category_element_tab = document.getElementsByClassName('bio_right_content')
 
 function fun(){
     if(y.checked==true){
@@ -9,3 +11,14 @@ function fun(){
         x.classList.remove('show')
     }
 }
+
+function category(value){
+        for(cat of category_element){
+            cat.classList.remove('active_link')
+        }
+        for(content_tab of category_element_tab){
+            content_tab.classList.remove('active_tab')
+        }
+        event.currentTarget.classList.add('active_link')
+        document.getElementById(value).classList.add('active_tab')
+    }
